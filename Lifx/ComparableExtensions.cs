6c7fb34adc16372a6f2aef64cf022bac193b3db3
Ollie Day <OllieDay@users.noreply.github.com>
2017-04-03 19:41:08 +0100

    Add extension method for comparing range of IComparable<T>

---
+++ Lifx/ComparableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lifx
+{
+	internal static class ComparableExtensions
+	{
+		// Check if value is within specified range (inclusive).
+		public static bool InRange<T>(this T @this, T from, T to) where T : IComparable<T>
+		{
+			return @this.CompareTo(from) >= 0 && @this.CompareTo(to) <= 0;
+		}
+	}
+}
