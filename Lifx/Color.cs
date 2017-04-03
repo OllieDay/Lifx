e5f14ce1620dcc90a4278fe203e772f778f8842a
Ollie Day <OllieDay@users.noreply.github.com>
2017-04-03 19:40:28 +0100

    Add Color class

---
+++ Lifx/Color.cs
@@ -0,0 +1,22 @@
+﻿namespace Lifx
+{
+	// Represents a color comprised of hue and saturation.
+	public struct Color
+	{
+		public Color(Hue hue, Percentage saturation)
+		{
+			Hue = hue;
+			Saturation = saturation;
+		}
+
+		public Hue Hue { get; }
+		public Percentage Saturation { get; }
+
+		internal static Color None { get; } = new Color(Hue.MinValue, Percentage.MinValue);
+
+		public override string ToString()
+		{
+			return $"[Hue: {Hue}; Saturation: {Saturation}]";
+		}
+	}
+}
