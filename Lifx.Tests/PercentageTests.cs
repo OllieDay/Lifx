1bb2314d80d39c9c19b7b654d427d64fdc7b0cbe
Ollie Day <OllieDay@users.noreply.github.com>
2017-04-03 19:36:35 +0100

    Add Percentage class

---
+++ Lifx.Tests/PercentageTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace Lifx.Tests
+{
+	public sealed class PercentageTests
+	{
+
+		[Theory]
+		[InlineData(-0.1)]
+		[InlineData(1.1)]
+		public void ConstructorShouldThrowArgumentOutOfRangeExceptionWhenValueNotInRange(double value)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => new Percentage(value));
+		}
+	}
+}
---
+++ Lifx/Percentage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lifx
+{
+	// Represents a percentage value from 0 to 1.
+	public struct Percentage
+	{
+		public Percentage(double value)
+		{
+			if (!value.InRange(MinValue, MaxValue))
+			{
+				throw new ArgumentOutOfRangeException(nameof(value), value, $"Must be {MinValue} to {MaxValue}.");
+			}
+
+			Value = value;
+		}
+
+		public static double MinValue => 0;
+		public static double MaxValue => 1;
+
+		public double Value { get; }
+
+		public static implicit operator double(Percentage percentage)
+		{
+			return percentage.Value;
+		}
+
+		public static implicit operator Percentage(double value)
+		{
+			return new Percentage(value);
+		}
+
+		public override string ToString()
+		{
+			return Value.ToString();
+		}
+	}
+}
