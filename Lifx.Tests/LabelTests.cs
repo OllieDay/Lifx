f4f1b014f9887ba872f7a59b9bf0968287720fb1
Ollie Day <OllieDay@users.noreply.github.com>
2017-04-03 19:45:11 +0100

    Add Label class

---
+++ Lifx.Tests/LabelTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Lifx.Tests
+{
+	public sealed class LabelTests
+	{
+		[Theory]
+		[InlineData("000000000000000000000000000000000")]
+		[InlineData("零零零零零零零零零零零")]
+		public void ConstructorShouldThrowArgumentExceptionWhenNumberOfBytesExceedsMaxLength(string value)
+		{
+			Assert.Throws<ArgumentException>(() => new Label(value));
+		}
+	}
+}
---
+++ Lifx/Label.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Lifx
+{
+	// Represents a light label with a maximum of 32 bytes in length.
+	public struct Label
+	{
+		public Label(string value)
+		{
+			if (Encoding.UTF8.GetByteCount(value) > MaxLength)
+			{
+				throw new ArgumentException($"Size in bytes must not exceed {MaxLength}.", nameof(value));
+			}
+
+			Value = value;
+		}
+
+		public static int MaxLength => 32;
+
+		public string Value { get; }
+
+		internal static Label None => string.Empty;
+
+		public static implicit operator string(Label label)
+		{
+			return label.Value;
+		}
+
+		public static implicit operator Label(string value)
+		{
+			return new Label(value);
+		}
+
+		public override string ToString()
+		{
+			return Value;
+		}
+	}
+}
