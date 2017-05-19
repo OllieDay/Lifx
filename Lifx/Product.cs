0e17f9848f6a6699972577cd373e3097a7f90751
Ollie Day <OllieDay@users.noreply.github.com>
2017-05-19 20:39:08 +0100

    Add Product enum representing type of light

---
+++ Lifx/Product.cs
@@ -0,0 +1,18 @@
+namespace Lifx
+{
+	// Represents the Lifx product type.
+	internal enum Product : uint
+	{
+		Unknown = 0,
+		Original1000 = 1,
+		Color650 = 3,
+		White800LowVoltage = 10,
+		White800HightVoltage = 11,
+		White900BR30 = 18,
+		Color1000BR30 = 20,
+		Color1000 = 22,
+		LifxPlusA19 = 29,
+		LifxPlusBR30 = 30,
+		LifxZ = 31
+	}
+}
