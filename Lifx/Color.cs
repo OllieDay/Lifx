5a07249e8856accdef3f607d6bacedcdc76c83c6
Ollie Day <OllieDay@users.noreply.github.com>
2017-06-05 21:50:24 +0100

    Add named colors

--- Lifx/Color.cs
+++ Lifx/Color.cs
@@ -9,6 +9,16 @@
 			Saturation = saturation;
 		}
 
+		public static Color White { get; } = new Color(0, 0);
+		public static Color Red { get; } = new Color(0, 1);
+		public static Color Orange { get; } = new Color(36, 1);
+		public static Color Yellow { get; } = new Color(60, 1);
+		public static Color Green { get; } = new Color(120, 1);
+		public static Color Cyan { get; } = new Color(180, 1);
+		public static Color Blue { get; } = new Color(250, 1);
+		public static Color Purple { get; } = new Color(280, 1);
+		public static Color Pink { get; } = new Color(325, 1);
+
 		public Hue Hue { get; }
 		public Percentage Saturation { get; }
 
