017cf6f10e484c5f162442ec7479f4b1b693fb8d
Ollie Day <OllieDay@users.noreply.github.com>
2024-10-21 23:13:27 +0100

    Fix test

--- Lifx.Tests/TemperatureTests.cs
+++ Lifx.Tests/TemperatureTests.cs
@@ -3,7 +3,7 @@ namespace Lifx.Tests;
 public sealed class TemperatureTests
 {
 	[Theory]
-	[InlineData(2499)]
+	[InlineData(1499)]
 	[InlineData(9001)]
 	public void ConstructorShouldThrowArgumentOutOfRangeExceptionWhenValueNotInRange(int value)
 		=> Assert.Throws<ArgumentOutOfRangeException>(() => new Temperature(value));
